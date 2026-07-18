3ad1d9123379544bcfd398e4eff4d9326b8c5fea
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-07-18 21:38:11 +0530

    updates

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -1,14 +1,27 @@
 const mongoose = require("mongoose");
 
+// // Step:1 (compass)
+// const dbConnection = async () => {
+//     try {
+// // We are connecting the DB_URL from the .env file
+//         await mongoose.connect(process.env.DB_URL);
+//         console.log("🔥 Database Connected to MongoDB Compass(localhost) Successfully!");
+//     } catch(err) {
+//         console.log("❌ Database Connection Failed:", err);
+//         process.exit(1);
+//     }
+// };
+
+
+// step:2 (atlas)
 const dbConnection = async () => {
     try {
-// We are connecting the DB_URL from the .env file
-        await mongoose.connect(process.env.DB_URL);
-        console.log("🔥 MongoDB Database Connected Successfully!");
+// We are connecting the atlasport from the .env file
+        await mongoose.connect(process.env.atlasport);
+        console.log("🔥 Database Connected to MongoDB Atlas Successfully!");
     } catch(err) {
         console.log("❌ Database Connection Failed:", err);
         process.exit(1);
     }
 };
-
 module.exports = dbConnection;
--- server/main.js
+++ server/main.js
@@ -4,6 +4,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const rateLimit = require('express-rate-limit');
+const dns = require("dns");
+
+// dns.setServers(["1.1.1.1", "8.8.8.8"]);
+if (process.env.NODE_ENV === "development") {
+    dns.setServers(["1.1.1.1", "8.8.8.8"]);
+}
+
+const errorMiddleware = require('./middleware/error.middleware');
 
 // 🆕 Bringing in the database connection
 const dbConnection = require("./config/dbconnection");
@@ -13,9 +21,20 @@ dbConnection(); // Connecting!
 // Initialize Express App
 const app = express();
 
-// Middleware
+
 app.use(express.json()); // To parse JSON bodies
 app.use(cors()); // To allow cross-origin requests from React
+
+// Rate Limiter Configuration
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again after 5 minutes."
+});
+
+// Apply rate limiter to all API routes
+app.use("/api", limiter);
+
 
 // Basic Route for Testing
 app.get('/', (req, res) => {
@@ -27,21 +46,11 @@ app.get('/', (req, res) => {
 const recipeRoutes = require("./routes/recipe.routes");
 const authRoutes = require("./routes/auth.routes");
 
-const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  message: "Too many requests, please try again after 5 minutes."
-});
-
 // Apply to all API routes
-app.use("/api", limiter);
 app.use("/api", recipeRoutes); // /api will appear before every URL
 app.use("/api/auth", authRoutes); // 🆕 Auth Routes Link
 
-app.get('/', (req, res) => {
-    res.send("RecipeShare Backend API is running perfectly! 🚀");
-});
-
+app.use(errorMiddleware);
 
 // Start Server
 const PORT = process.env.PORT || 8080;

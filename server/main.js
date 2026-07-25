5ee2af178de4a1fa72da74db53b88a5c7a6ad68c
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-07-25 23:57:07 +0530

    updates

--- server/main.js
+++ server/main.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 // Rate Limiter Configuration
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 10, // limit each IP to 10 requests per windowMs
     message: "Too many requests, please try again after 5 minutes."
 });
 
